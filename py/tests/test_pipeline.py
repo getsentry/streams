d8d376122601a9bd91be22e351dcf133e474fcdb
Riya Chakraborty <riya.chakraborty@sentry.io>
2025-02-12 14:56:33 -0800

    somehow mypy is happy

--- py/tests/test_pipeline.py
+++ py/tests/test_pipeline.py
@@ -1,7 +1,8 @@
 import json
 import os
-import unittest
+from typing import Any, Generator, MutableMapping
 
+import pytest
 from pyflink.datastream import StreamExecutionEnvironment
 from sentry_streams.adapters.stream_adapter import RuntimeTranslator, StreamAdapter
 from sentry_streams.flink.flink_adapter import FlinkAdapter
@@ -9,118 +10,156 @@ from sentry_streams.pipeline import KafkaSink, KafkaSource, Map, Pipeline
 from sentry_streams.runner import iterate_edges
 
 
-class PipelineTestCase(unittest.TestCase):
+@pytest.fixture(autouse=True)
+def setup_function() -> Generator[tuple[StreamExecutionEnvironment, RuntimeTranslator]]:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    libs_path = os.path.join("/".join(dir_path.split("/")[:-2]), "flink_libs")
+    assert libs_path is not None, "FLINK_LIBS environment variable is not set"
 
-    def setUp(self) -> None:
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        libs_path = os.path.join("/".join(dir_path.split("/")[:-2]), "flink_libs")
-        assert libs_path is not None, "FLINK_LIBS environment variable is not set"
+    jar_file = os.path.join(os.path.abspath(libs_path), "flink-connector-kafka-3.4.0-1.20.jar")
+    kafka_jar_file = os.path.join(os.path.abspath(libs_path), "kafka-clients-3.4.0.jar")
 
-        jar_file = os.path.join(os.path.abspath(libs_path), "flink-connector-kafka-3.4.0-1.20.jar")
-        kafka_jar_file = os.path.join(os.path.abspath(libs_path), "kafka-clients-3.4.0.jar")
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+    env.add_jars(f"file://{jar_file}", f"file://{kafka_jar_file}")
 
-        self.env = StreamExecutionEnvironment.get_execution_environment()
-        self.env.set_parallelism(1)
-        self.env.add_jars(f"file://{jar_file}", f"file://{kafka_jar_file}")
+    # TODO: read from yaml file
+    environment_config = {
+        "topics": {
+            "logical-events": "events",
+            "transformed-events": "transformed-events",
+        },
+        "broker": "localhost:9092",
+    }
 
-        # TODO: read from yaml file
-        environment_config = {
-            "topics": {
-                "logical-events": "events",
-                "transformed-events": "transformed-events",
+    runtime_config: StreamAdapter = FlinkAdapter(environment_config, env)
+    translator = RuntimeTranslator(runtime_config)
+
+    setup_data = (env, translator)
+
+    yield setup_data
+
+
+def basic() -> tuple[Pipeline, MutableMapping[str, list[dict[str, Any]]]]:
+    pipeline = Pipeline()
+
+    source = KafkaSource(
+        name="myinput",
+        ctx=pipeline,
+        logical_topic="logical-events",
+    )
+
+    _ = KafkaSink(
+        name="kafkasink",
+        ctx=pipeline,
+        inputs=[source],
+        logical_topic="transformed-events",
+    )
+
+    expected = {
+        "nodes": [
+            {
+                "id": 1,
+                "type": "Source: Custom Source",
+                "pact": "Data Source",
+                "contents": "Source: Custom Source",
+                "parallelism": 1,
             },
-            "broker": "localhost:9092",
-        }
+            {
+                "id": 3,
+                "type": "Sink: Writer",
+                "pact": "Operator",
+                "contents": "Sink: Writer",
+                "parallelism": 1,
+                "predecessors": [{"id": 1, "ship_strategy": "FORWARD", "side": "second"}],
+            },
+            {
+                "id": 5,
+                "type": "Sink: Committer",
+                "pact": "Operator",
+                "contents": "Sink: Committer",
+                "parallelism": 1,
+                "predecessors": [{"id": 3, "ship_strategy": "FORWARD", "side": "second"}],
+            },
+        ]
+    }
 
-        runtime_config: StreamAdapter = FlinkAdapter(environment_config, self.env)
-        self.translator = RuntimeTranslator(runtime_config)
+    return (pipeline, expected)
 
-    def test_pipeline(self) -> None:
 
-        pipeline = Pipeline()
+def basic_map() -> tuple[Pipeline, MutableMapping[str, list[dict[str, Any]]]]:
+    pipeline = Pipeline()
 
-        source = KafkaSource(
-            name="myinput",
-            ctx=pipeline,
-            logical_topic="logical-events",
-        )
+    source = KafkaSource(
+        name="myinput",
+        ctx=pipeline,
+        logical_topic="logical-events",
+    )
 
-        map = Map(
-            name="mymap",
-            ctx=pipeline,
-            inputs=[source],
-            function="sentry_streams.sample_function.EventsPipelineMapFunction.simple_map",
-        )
+    map = Map(
+        name="mymap",
+        ctx=pipeline,
+        inputs=[source],
+        function="sentry_streams.sample_function.EventsPipelineMapFunction.simple_map",
+    )
 
-        _ = KafkaSink(
-            name="kafkasink",
-            ctx=pipeline,
-            inputs=[map],
-            logical_topic="transformed-events",
-        )
+    _ = KafkaSink(
+        name="kafkasink",
+        ctx=pipeline,
+        inputs=[map],
+        logical_topic="transformed-events",
+    )
 
-        iterate_edges(pipeline, self.translator)
+    expected = {
+        "nodes": [
+            {
+                "id": 7,
+                "type": "Source: Custom Source",
+                "pact": "Data Source",
+                "contents": "Source: Custom Source",
+                "parallelism": 1,
+            },
+            {
+                "id": 8,
+                "type": "Map",
+                "pact": "Operator",
+                "contents": "Map",
+                "parallelism": 1,
+                "predecessors": [{"id": 7, "ship_strategy": "FORWARD", "side": "second"}],
+            },
+            {
+                "id": 10,
+                "type": "Sink: Writer",
+                "pact": "Operator",
+                "contents": "Sink: Writer",
+                "parallelism": 1,
+                "predecessors": [{"id": 8, "ship_strategy": "FORWARD", "side": "second"}],
+            },
+            {
+                "id": 12,
+                "type": "Sink: Committer",
+                "pact": "Operator",
+                "contents": "Sink: Committer",
+                "parallelism": 1,
+                "predecessors": [{"id": 10, "ship_strategy": "FORWARD", "side": "second"}],
+            },
+        ]
+    }
 
-        expected_plan = {
-            "nodes": [
-                {
-                    "id": 1,
-                    "type": "Source: Custom Source",
-                    "pact": "Data Source",
-                    "contents": "Source: Custom Source",
-                    "parallelism": 1,
-                },
-                {
-                    "id": 2,
-                    "type": "Map",
-                    "pact": "Operator",
-                    "contents": "Map",
-                    "parallelism": 1,
-                    "predecessors": [{"id": 1, "ship_strategy": "FORWARD", "side": "second"}],
-                },
-                {
-                    "id": 4,
-                    "type": "Sink: Writer",
-                    "pact": "Operator",
-                    "contents": "Sink: Writer",
-                    "parallelism": 1,
-                    "predecessors": [{"id": 2, "ship_strategy": "FORWARD", "side": "second"}],
-                },
-                {
-                    "id": 6,
-                    "type": "Sink: Committer",
-                    "pact": "Operator",
-                    "contents": "Sink: Committer",
-                    "parallelism": 1,
-                    "predecessors": [{"id": 4, "ship_strategy": "FORWARD", "side": "second"}],
-                },
-            ]
-        }
-        self.env.get_execution_plan() == json.dumps(expected_plan)
+    return (pipeline, expected)
 
-    def test_map_bad_module(self) -> None:
 
-        pipeline = Pipeline()
+@pytest.mark.parametrize("pipeline,expected_plan", [basic(), basic_map()])
+def test_pipeline(
+    setup_function: tuple[StreamExecutionEnvironment, RuntimeTranslator],
+    pipeline: Pipeline,
+    expected_plan: MutableMapping[str, list[dict[str, Any]]],
+) -> None:
+    env: StreamExecutionEnvironment
+    translator: RuntimeTranslator
 
-        source = KafkaSource(
-            name="myinput",
-            ctx=pipeline,
-            logical_topic="logical-events",
-        )
+    env, translator = setup_function
 
-        map = Map(
-            name="mymap",
-            ctx=pipeline,
-            inputs=[source],
-            function="sentry_streams.hello.EventsPipelineMapFunction.simple_map",
-        )
+    iterate_edges(pipeline, translator)
 
-        _ = KafkaSink(
-            name="kafkasink",
-            ctx=pipeline,
-            inputs=[map],
-            logical_topic="transformed-events",
-        )
-
-        with self.assertRaises(ImportError):
-            iterate_edges(pipeline, self.translator)
+    assert json.loads(env.get_execution_plan()) == expected_plan
