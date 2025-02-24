0a7787f66a797c662e082658b799046c7b6da305
Filippo Pacifici <fpacifici@sentry.io>
2025-02-24 15:36:59 -0800

    Decouple Flink initialization from the runner (#22)
    * Hide Flink initialization
    
    * Add dynamic loader
    
    * Add lib path
    
    * Remove print statement

--- py/sentry_streams/flink/flink_adapter.py
+++ py/sentry_streams/flink/flink_adapter.py
@@ -1,4 +1,5 @@
-from typing import Any, MutableMapping
+import os
+from typing import Any, Self
 
 from pyflink.common import Types
 from pyflink.common.serialization import SimpleStringSchema
@@ -11,7 +12,7 @@ from pyflink.datastream.connectors.kafka import (
     KafkaSink,
 )
 
-from sentry_streams.adapters.stream_adapter import StreamAdapter
+from sentry_streams.adapters.stream_adapter import PipelineConfig, StreamAdapter
 from sentry_streams.modules import get_module
 from sentry_streams.pipeline import Map, Step
 
@@ -25,9 +26,32 @@ class FlinkAdapter(StreamAdapter):
     # that send data to a next step that
     # performs serialization (e.g. Map --> Sink)
 
-    def __init__(self, config: MutableMapping[str, Any], env: StreamExecutionEnvironment) -> None:
+    def __init__(self, config: PipelineConfig, env: StreamExecutionEnvironment) -> None:
         self.environment_config = config
         self.env = env
+
+    @classmethod
+    def build(cls, config: PipelineConfig) -> Self:
+        env = StreamExecutionEnvironment.get_execution_environment()
+
+        flink_config = config.get("flink", {})
+
+        libs_path = flink_config.get("kafka_connect_lib_path")
+        if libs_path is None:
+            libs_path = os.environ.get("FLINK_LIBS")
+
+        if libs_path:
+            jar_file = os.path.join(
+                os.path.abspath(libs_path), "flink-sql-connector-kafka-3.4.0-1.20.jar"
+            )
+            kafka_jar_file = os.path.join(
+                os.path.abspath(libs_path), "flink-sql-connector-kafka-3.4.0-1.20.jar"
+            )
+            env.add_jars(f"file://{jar_file}", f"file://{kafka_jar_file}")
+
+        env.set_parallelism(flink_config.get("parallelism", 1))
+
+        return cls(config, env)
 
     def source(self, step: Step) -> Any:
         assert hasattr(step, "logical_topic")
@@ -83,3 +107,6 @@ class FlinkAdapter(StreamAdapter):
 
         # TODO: Ensure output type is configurable like the schema above
         return stream.map(func=lambda msg: imported_fn(msg), output_type=Types.STRING())
+
+    def run(self) -> None:
+        self.env.execute()
