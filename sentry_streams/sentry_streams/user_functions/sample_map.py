3786c9d8d3e6bda62406b4fbd30f214b53b3f30b
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2025-03-06 16:33:55 -0500

    ref: split user_functions (#53)

---
+++ sentry_streams/sentry_streams/user_functions/sample_map.py
@@ -0,0 +1,15 @@
+import json
+
+
+class EventsPipelineMapFunctions:
+    """
+    Sample user-defined functions to
+    plug into pipeline
+    """
+
+    @staticmethod
+    def simple_map(value: str) -> str:
+        d = json.loads(value)
+        res: str = d.get("name", "no name")
+
+        return "hello " + res
