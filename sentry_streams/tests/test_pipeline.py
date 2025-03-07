61e90c33e98925792b8c4ce4b04a31418bf555d9
victoria-yining-huang <victoria.huang777@gmail.com>
2025-03-07 14:24:36 -0500

    make fixture

--- sentry_streams/tests/test_pipeline.py
+++ sentry_streams/tests/test_pipeline.py
@@ -1,4 +1,4 @@
-import json
+import pytest
 
 from sentry_streams.pipeline.pipeline import (
     Filter,
@@ -9,80 +9,79 @@ from sentry_streams.pipeline.pipeline import (
     Step,
 )
 
-pipeline = Pipeline()
+
+@pytest.fixture
+def pipeline() -> Pipeline:
+    pipeline = Pipeline()
+    source = KafkaSource(
+        name="myinput",
+        ctx=pipeline,
+        logical_topic="logical-events",
+    )
+
+    source_2 = KafkaSource(
+        name="myinput2",
+        ctx=pipeline,
+        logical_topic="anotehr-logical-events",
+    )
+
+    filter = Filter(
+        name="myfilter",
+        ctx=pipeline,
+        inputs=[source, source_2],
+        function=simple_filter,
+    )
+
+    _ = Filter(
+        name="myfilter2",
+        ctx=pipeline,
+        inputs=[filter],
+        function=simple_filter,
+    )
+
+    map = Map(
+        name="mymap",
+        ctx=pipeline,
+        inputs=[filter],
+        function=simple_map,
+    )
+
+    map_2 = Map(
+        name="mymap2",
+        ctx=pipeline,
+        inputs=[filter],
+        function=simple_map,
+    )
+
+    KafkaSink(
+        name="kafkasink",
+        ctx=pipeline,
+        inputs=[map, map_2],
+        logical_topic="transformed-events",
+    )
+    return pipeline
 
 
 def simple_filter(value: str) -> bool:
-    d = json.loads(value)
-    return True if "name" in d else False
+    # does nothing because it's not needed for tests
+    return True
 
 
 def simple_map(value: str) -> str:
-    d = json.loads(value)
-    res: str = d.get("name", "no name")
-
-    return "hello " + res
+    # does nothing because it's not needed for tests
+    return "nothing"
 
 
-source = KafkaSource(
-    name="myinput",
-    ctx=pipeline,
-    logical_topic="logical-events",
-)
-
-source_2 = KafkaSource(
-    name="myinput2",
-    ctx=pipeline,
-    logical_topic="anotehr-logical-events",
-)
-
-
-filter = Filter(
-    name="myfilter",
-    ctx=pipeline,
-    inputs=[source, source_2],
-    function=simple_filter,
-)
-
-filter_2 = Filter(
-    name="myfilter2",
-    ctx=pipeline,
-    inputs=[filter],
-    function=simple_filter,
-)
-
-map = Map(
-    name="mymap",
-    ctx=pipeline,
-    inputs=[filter],
-    function=simple_map,
-)
-
-map_2 = Map(
-    name="mymap2",
-    ctx=pipeline,
-    inputs=[filter],
-    function=simple_map,
-)
-
-sink = KafkaSink(
-    name="kafkasink",
-    ctx=pipeline,
-    inputs=[map, map_2],
-    logical_topic="transformed-events",
-)
-
-
-def test_register_step() -> None:
+def test_register_step(pipeline: Pipeline) -> None:
     step = Step("new_step", pipeline)
     assert "new_step" in pipeline.steps
     assert pipeline.steps["new_step"] == step
 
 
-def test_register_edge() -> None:
+def test_register_edge(pipeline: Pipeline) -> None:
     assert pipeline.outgoing_edges["myfilter"] == ["myfilter2", "mymap", "mymap2"]
     assert pipeline.incoming_edges["myfilter"] == ["myinput", "myinput2"]
 
 
-def test_register_source() -> None:
-    assert source in pipeline.sources
+def test_register_source(pipeline: Pipeline) -> None:
+    assert {pipeline.sources[0].name, pipeline.sources[1].name} == {"myinput", "myinput2"}
