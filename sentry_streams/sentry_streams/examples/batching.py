1c5cdeaba12a029e30e465f4ee1f325980af4bbe
Riya Chakraborty <riya.chakraborty@sentry.io>
2025-03-11 17:20:51 -0700

    local unbatch

--- sentry_streams/sentry_streams/examples/batching.py
+++ sentry_streams/sentry_streams/examples/batching.py
@@ -1,10 +1,9 @@
-from sentry_streams.examples.batch_builder import build_batch_str
 from sentry_streams.pipeline.pipeline import (
     Batch,
     KafkaSink,
     KafkaSource,
-    Map,
     Pipeline,
+    Unbatch,
 )
 
 pipeline = Pipeline()
@@ -21,12 +20,18 @@ source = KafkaSource(
 
 reduce: Batch[int, str] = Batch(name="mybatch", ctx=pipeline, inputs=[source], batch_size=5)
 
-map = Map(name="mymap", ctx=pipeline, inputs=[reduce], function=build_batch_str)
+unbatch: Unbatch[str] = Unbatch(name="myunbatch", ctx=pipeline, inputs=[reduce])
+
+# map = Map(name="mymap",
+#     ctx=pipeline,
+#     inputs=[unbatch],
+#     function=build_batch_str
+# )
 
 # flush the batches to the Sink
 sink = KafkaSink(
     name="kafkasink",
     ctx=pipeline,
-    inputs=[map],
+    inputs=[unbatch],
     logical_topic="transformed-events",
 )
