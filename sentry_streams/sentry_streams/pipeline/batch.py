62a601acbc9dc84199728b1ecaf1b6cbce4e96e6
Riya Chakraborty <riya.chakraborty@sentry.io>
2025-03-12 12:44:00 -0700

    switch to MutableSequence

--- sentry_streams/sentry_streams/pipeline/batch.py
+++ sentry_streams/sentry_streams/pipeline/batch.py
@@ -1,23 +1,23 @@
-from typing import Generator, Self
+from typing import Generator, MutableSequence, Self
 
 from sentry_streams.pipeline.function_template import Accumulator, InputType
 
 
-class BatchBuilder(Accumulator[InputType, list[InputType]]):
+class BatchBuilder(Accumulator[InputType, MutableSequence[InputType]]):
     """
     Takes str input and accumulates them into a batch array.
     Joins back into a string to produce onto a Kafka topic.
     """
 
     def __init__(self) -> None:
-        self.batch: list[InputType] = []
+        self.batch: MutableSequence[InputType] = []
 
     def add(self, value: InputType) -> Self:
         self.batch.append(value)
 
         return self
 
-    def get_value(self) -> list[InputType]:
+    def get_value(self) -> MutableSequence[InputType]:
         return self.batch
 
     def merge(self, other: Self) -> Self:
@@ -26,6 +26,6 @@ class BatchBuilder(Accumulator[InputType, list[InputType]]):
         return self
 
 
-def unbatch(batch: list[InputType]) -> Generator[InputType, None, None]:
+def unbatch(batch: MutableSequence[InputType]) -> Generator[InputType, None, None]:
     for message in batch:
         yield message
