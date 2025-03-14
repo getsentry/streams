fad231367eaa2c4a4a549fa7e592ab078d688418
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2025-03-14 16:55:26 -0400

    Revert "use float for timestamp type"
    This reverts commit c5778370db6640d1f0a705ea1fac483d3fff604d.

--- sentry_streams/sentry_streams/examples/blq_fn.py
+++ sentry_streams/sentry_streams/examples/blq_fn.py
@@ -1,10 +1,7 @@
 import json
-import time
 from dataclasses import dataclass
+from datetime import datetime, timedelta, timezone
 from enum import Enum
-
-# 10 minutes
-MAX_MESSAGE_DELAY = 600000
 
 
 class DownstreamBranch(Enum):
@@ -13,22 +10,26 @@ class DownstreamBranch(Enum):
 
 
 @dataclass
-class Message:
+class KafkaMessage:
+    key: str
+    headers: dict[str, str]
     value: str
-    timestamp: float
+    timestamp: datetime
 
 
-def unpack_kafka_message(msg: str) -> Message:
+def unpack_kafka_message(msg: str) -> KafkaMessage:
     d = json.loads(msg)
-    return Message(
+    return KafkaMessage(
+        key=d["key"],
+        headers=d["headers"],
         value=d["value"],
-        timestamp=d["timestamp"],
+        timestamp=datetime.fromisoformat(d["timestamp"]),
     )
 
 
-def should_send_to_blq(msg: Message) -> DownstreamBranch:
+def should_send_to_blq(msg: KafkaMessage) -> DownstreamBranch:
     timestamp = msg.timestamp
-    if timestamp < time.time() - MAX_MESSAGE_DELAY:
+    if timestamp < datetime.now(timezone.utc) - timedelta(minutes=10):
         return DownstreamBranch.DELAYED
     else:
         return DownstreamBranch.RECENT
