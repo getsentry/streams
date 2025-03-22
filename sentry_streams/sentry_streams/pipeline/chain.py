0be0071a7ff12d201171970b87e1d270947f09ee
Filippo Pacifici <fpacifici@sentry.io>
2025-03-21 17:50:26 -0700

    Add docs

--- sentry_streams/sentry_streams/pipeline/chain.py
+++ sentry_streams/sentry_streams/pipeline/chain.py
@@ -46,8 +46,24 @@ TOut = TypeVar("TOut")
 
 @dataclass
 class Applier(ABC, Generic[TIn, TOut]):
+    """
+    Defines a primitive that can be applied on a stream.
+    Instances of these class represent a step in the pipeline and
+    contains the metadata needed by the adapter to add the step
+    to the pipeline itself.
+
+    This class is primarily syntactic sugar to avoid having tons
+    of methods in the `Chain` class and still allow some customization
+    of the primitives.
+    """
+
     @abstractmethod
     def build_step(self, name: str, ctx: Pipeline, previous: Step) -> Step:
+        """
+        Build a pipeline step and wires it to the Pipeline.
+
+        This method will go away once the old syntax will be retired.
+        """
         raise NotImplementedError
 
 
@@ -110,13 +126,50 @@ class Batch(
         )
 
 
-class TerminatedChain(Pipeline):
+class Chain(Pipeline):
+    """
+    A pipeline that terminates with a branch or a sink. Which means a pipeline
+    we cannot append further steps on.
+
+    This type exists so the type checker can prevent us from reaching an
+    invalid state.
+    """
+
     def __init__(self, name: str) -> None:
         super().__init__()
         self.name = name
 
 
-class Chain(TerminatedChain):
+class ExtensibleChain(Chain):
+    """
+    Defines a streaming pipeline or a segment of a pipeline by chaining
+    operators that define steps via function calls.
+
+    A Chain is a pipeline that starts with a source, follows a number of
+    steps. Some steps are operators that perform processing on a stream.
+    Other steps manage the pipeline topology: sink, broadcast, route.
+
+    Example:
+    ```
+    pipeline = (
+        streaming_source("myinput", "events") # Starts the pipeline
+        .apply("transform1", Map(lambda msg: msg)) # Performs an operation
+        .route( # Branches the pipeline
+            "route_to_one",
+            routing_function=routing_func,
+            routes={
+                Routes.ROUTE1: segment(name="route1") # Creates a branch
+                .apply("transform2", Map(lambda msg: msg))
+                .sink("myoutput1", "transformed-events2"),
+                Routes.ROUTE2: segment(name="route2")
+                .apply("transform3", Map(lambda msg: msg))
+                .sink("myoutput2", "transformed-events3"),
+            },
+        )
+    )
+    ```
+    """
+
     def __init__(self, name: str) -> None:
         super().__init__(name)
         self.__edge: Step | None = None
@@ -124,12 +177,26 @@ class Chain(TerminatedChain):
     def _add_start(self, start: Step) -> None:
         self.__edge = start
 
-    def apply(self, name: str, applier: Applier[TIn, TOut]) -> Chain:
+    def apply(self, name: str, applier: Applier[TIn, TOut]) -> ExtensibleChain:
+        """
+        Apply a transformation to the stream. The transformation is
+        defined via an `Applier`.
+
+        Operations can change the cardinality of the messages in the stream.
+        Examples:
+        - map performs a 1:1 transformation
+        - filter performs a 1:0..1 transformation
+        - flatMap performs a 1:n transformation
+        - reduce performs a n:1 transformation
+        """
         assert self.__edge is not None
         self.__edge = applier.build_step(name, self, self.__edge)
         return self
 
-    def broadcast(self, name: str, routes: Sequence[TerminatedChain]) -> TerminatedChain:
+    def broadcast(self, name: str, routes: Sequence[Chain]) -> Chain:
+        """
+        Forks the pipeline sending all messages to all routes.
+        """
         assert self.__edge is not None
         for chain in routes:
             self.merge(other=chain, merge_point=self.__edge.name)
@@ -139,8 +206,13 @@ class Chain(TerminatedChain):
         self,
         name: str,
         routing_function: Callable[..., TRoute],
-        routes: Mapping[TRoute, TerminatedChain],
-    ) -> TerminatedChain:
+        routes: Mapping[TRoute, Chain],
+    ) -> Chain:
+        """
+        Forks the pipeline sending each message to one of the branches.
+        The `routing_function` parameter specifies the function that
+        takes the message in and returns the route to send it to.
+        """
         assert self.__edge is not None
         table = {branch: Branch(name=chain.name, ctx=self) for branch, chain in routes.items()}
         Router(
@@ -156,20 +228,32 @@ class Chain(TerminatedChain):
 
         return self
 
-    def sink(self, name: str, stream_name: str) -> TerminatedChain:
+    def sink(self, name: str, stream_name: str) -> Chain:
+        """
+        Terminates the pipeline.
+
+        TODO: support anything other than StreamSink.
+        """
         assert self.__edge is not None
         StreamSink(name=name, ctx=self, inputs=[self.__edge], stream_name=stream_name)
         return self
 
 
-def segment(name: str) -> Chain:
-    pipeline: Chain = Chain(name)
+def segment(name: str) -> ExtensibleChain:
+    """
+    Creates a segment of a pipeline to be referenced in existing pipelines
+    in route and broadcast steps.
+    """
+    pipeline: ExtensibleChain = ExtensibleChain(name)
     pipeline._add_start(Branch(name=name, ctx=pipeline))
     return pipeline
 
 
-def streaming_source(name: str, stream_name: str) -> Chain:
-    pipeline: Chain = Chain("root")
+def streaming_source(name: str, stream_name: str) -> ExtensibleChain:
+    """
+    Create a pipeline that starts with a StreamingSource.
+    """
+    pipeline: ExtensibleChain = ExtensibleChain("root")
     source = StreamSource(
         name=name,
         ctx=pipeline,
