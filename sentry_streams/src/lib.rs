00ec7535429d4e04fd16b4c515e3f4165cdecc35
Filippo Pacifici <fpacifici@sentry.io>
2025-03-27 16:05:15 -0700

    Some stuff

--- sentry_streams/src/lib.rs
+++ sentry_streams/src/lib.rs
@@ -1,14 +1,13 @@
 use pyo3::prelude::*;
 mod routes;
-mod steps;
 
 #[pymodule]
 fn rust_streams(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<routes::Route>()?;
     m.add_class::<routes::RoutedValue>()?;
 
-    let sub = PyModule::new(py, "steps")?;
-    steps::rust_steps(&sub)?;
-    m.add_submodule(&sub)?;
+    //let sub = PyModule::new(m.py(), "steps")?;
+    //steps::rust_steps(&sub)?;
+    //m.add_submodule(&sub)?;
     Ok(())
 }
