eaa96e5dd43f814e7eac7fa871329b39be19e724
Filippo Pacifici <fpacifici@sentry.io>
2025-04-01 23:15:49 -0700

    Add some content

--- sentry_streams/docs/source/conf.py
+++ sentry_streams/docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -11,10 +14,17 @@ copyright = "2025, blank"
 author = "blank"
 release = "0.1"
 
+sys.path.insert(0, os.path.abspath("../.."))
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinxcontrib.mermaid"]
+extensions = [
+    "sphinxcontrib.mermaid",
+    "sphinx.ext.autodoc",
+]
+
+always_document_param_types = True
 
 templates_path = ["_templates"]
 exclude_patterns = ["build"]
