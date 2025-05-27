e49e77fdcb91cccc39e7d0e3eab5bd1de22829b4
Riya Chakraborty <riya.chakraborty@sentry.io>
2025-05-27 11:51:04 -0700

    address comments

--- sentry_streams/src/gcs_writer.rs
+++ sentry_streams/src/gcs_writer.rs
@@ -2,8 +2,10 @@ use anyhow::anyhow;
 use pyo3::types::PyAnyMethods;
 use pyo3::types::PyBytes;
 use pyo3::Python;
+use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use reqwest::Client;
+use reqwest::ClientBuilder;
 use sentry_arroyo::processing::strategies::run_task_in_threads::{
     RunTaskError, RunTaskFunc, TaskRunner,
 };
@@ -11,49 +13,60 @@ use sentry_arroyo::processing::strategies::run_task_in_threads::{
 use crate::routes::RoutedValue;
 use pyo3::prelude::*;
 use sentry_arroyo::types::Message;
-pub struct GCSWriter {}
+pub struct GCSWriter {
+    client: Client,
+    url: String,
+}
 
 impl GCSWriter {
     pub fn new() -> Self {
-        GCSWriter {}
+        let bucket = "arroyo-artifacts";
+        let object = "uploaded-file.txt";
+
+        let client = ClientBuilder::new();
+        let url = format!(
+            "https://storage.googleapis.com/upload/storage/v1/b/{}/o?uploadType=media&name={}",
+            bucket, object
+        );
+
+        let access_token = std::env::var("GCP_ACCESS_TOKEN")
+            .expect("Set GCP_ACCESS_TOKEN env variable with GCP authorization token");
+
+        let mut headers = HeaderMap::with_capacity(2);
+        headers.insert(
+            AUTHORIZATION,
+            HeaderValue::from_str(&format!("Bearer {}", access_token)).unwrap(),
+        );
+        headers.insert(
+            CONTENT_TYPE,
+            HeaderValue::from_str("application/octet-stream").unwrap(),
+        );
+
+        let client = client.default_headers(headers).build().unwrap();
+
+        GCSWriter { client, url }
     }
 }
 
 fn to_bytes(payload: &RoutedValue) -> Vec<u8> {
     let payload = Python::with_gil(|py| {
         let payload = payload.payload.clone_ref(py);
-        // if payload.is_none(py) {
-        // prolly need to handle this case
-
         let py_bytes: &Bound<PyBytes> = payload.bind(py).downcast().unwrap();
         py_bytes.as_bytes().to_vec()
     });
     payload
 }
 
-// start by supporting bytes only
 impl TaskRunner<RoutedValue, RoutedValue, anyhow::Error> for GCSWriter {
     fn get_task(&self, message: Message<RoutedValue>) -> RunTaskFunc<RoutedValue, anyhow::Error> {
+        let client = self.client.clone();
+        let url = self.url.clone();
+
         Box::pin(async move {
-            let bucket = "arroyo-artifacts";
-            let object = "uploaded-file.txt";
-
-            // Read the access token
-            let access_token = std::env::var("GCP_ACCESS_TOKEN")
-                .expect("Set GCP_ACCESS_TOKEN env variable with GCP authorization token");
-
             let bytes = to_bytes(message.payload());
-
-            let client = Client::new();
-            let url = format!(
-                "https://storage.googleapis.com/upload/storage/v1/b/{}/o?uploadType=media&name={}",
-                bucket, object
-            );
 
             client
                 .post(&url)
-                .header(AUTHORIZATION, format!("Bearer {}", access_token))
-                .header(CONTENT_TYPE, "application/octet-stream")
                 .body(bytes)
                 .send()
                 .await
