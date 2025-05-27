be452096f6797f0f73493f1b7b139e94ceb411d0
Filippo Pacifici <fpacifici@sentry.io>
2025-05-26 21:56:25 -0700

    Add docs

--- sentry_streams/sentry_streams/adapters/arroyo/reduce_delegate.py
+++ sentry_streams/sentry_streams/adapters/arroyo/reduce_delegate.py
@@ -33,7 +33,19 @@ TOut = TypeVar("TOut")
 
 
 class OutputRetriever(ProcessingStrategy[Union[FilteredPayload, TIn]]):
-    """ """
+    """
+    This is an Arroyo strategy to be wired to another strategy used inside
+    a `RustOperatorDelegate`. This strategy collects the result and return it to the
+    Rust code.
+
+    Arroyo strategies are provided the following step and are expected to
+    hand the result directly to it. This does not work for `RustOperatorDelegate`
+    which is expected to return the result as return value of poll and flush.
+
+    In order to wrap an existing Arroyo strategy in a `RustOperatorDelegate` we
+    need to provide an instance of this class to the existing strategy to
+    collect the results and send it them back to Rust as `poll` return value.
+    """
 
     def __init__(
         self,
@@ -41,6 +53,13 @@ class OutputRetriever(ProcessingStrategy[Union[FilteredPayload, TIn]]):
         self.__pending_messages: MutableSequence[Tuple[Message[TIn], Committable]] = []
 
     def submit(self, message: ArroyoMessage[Union[FilteredPayload, TIn]]) -> None:
+        """
+        Accumulates messages provided by the previous step in the consumer.
+
+        Different types of reducers can provide RoutedValues or bare aggregated
+        data. So this class has to support both.
+        Messages are turned into `PyMessage` and stored in this format.
+        """
         if isinstance(message.payload, FilteredPayload):
             return
         else:
@@ -81,8 +100,6 @@ class OutputRetriever(ProcessingStrategy[Union[FilteredPayload, TIn]]):
     def fetch(self) -> Iterable[Tuple[Message[TIn], Committable]]:
         """
         Fetches the output messages from the processing strategy.
-        This method is a placeholder and should be implemented to return
-        the actual output messages.
         """
         ret = self.__pending_messages
         self.__pending_messages = []
@@ -90,6 +107,9 @@ class OutputRetriever(ProcessingStrategy[Union[FilteredPayload, TIn]]):
 
 
 class ReduceDelegateFactory(RustOperatorFactory[TIn, TOut], Generic[TIn, TOut]):
+    """
+    Creates a `ReduceDelegate`. This is the class to provide to the Rust runtime.
+    """
 
     def __init__(self, step: Reduce[Any, Any, Any]) -> None:
         super().__init__()
@@ -112,6 +132,17 @@ class ReduceDelegateFactory(RustOperatorFactory[TIn, TOut], Generic[TIn, TOut]):
 
 
 class ReduceDelegate(RustOperatorDelegate[TIn, TOut], Generic[TIn, TOut]):
+    """
+    Wraps the various types of Python Reduce steps to be used by the
+    Rust runtime. Eventually we will move the reduce logic itself to Rust.
+
+    It receives the messages to add to the accumulator with the `submit` method.
+    These messages are directly forwarded to the Reduce strategy after
+    turning them into `RoutedValue`.
+
+    Extracts the results from `OutputRetriever` after polling on the Reduce
+    strategy.
+    """
 
     def __init__(
         self,
@@ -131,6 +162,8 @@ class ReduceDelegate(RustOperatorDelegate[TIn, TOut], Generic[TIn, TOut]):
         }
         msg = ArroyoMessage(
             Value(
+                # TODO: Stop creating a `RoutedValue` and make the Reduce strategy
+                # accept `Message` directly.
                 RoutedValue(self.__route, message),
                 arroyo_committable,
                 datetime.fromtimestamp(message.timestamp) if message.timestamp else None,
