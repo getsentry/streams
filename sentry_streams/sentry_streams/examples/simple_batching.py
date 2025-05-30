3515fbe32ec934d185a0406f0fd7536b9bbfe75d
Filippo Pacifici <fpacifici@sentry.io>
2025-05-30 16:14:51 -0700

    Fix the type

--- sentry_streams/sentry_streams/examples/simple_batching.py
+++ sentry_streams/sentry_streams/examples/simple_batching.py
@@ -1,7 +1,7 @@
 from sentry_kafka_schemas.schema_types.ingest_metrics_v1 import IngestMetric
 
 from sentry_streams.pipeline import Batch, streaming_source
-from sentry_streams.pipeline.chain import Parser, Serializer
+from sentry_streams.pipeline.chain import Parser, Serializer, StreamSink
 
 pipeline = streaming_source(
     name="myinput",
@@ -13,5 +13,5 @@ chain1 = (
     pipeline.apply("parser", Parser(msg_type=IngestMetric))
     .apply("mybatch", Batch(batch_size=3))
     .apply("serializer", Serializer())
-    .sink("mysink", stream_name="transformed-events")
+    .sink("mysink", StreamSink(stream_name="transformed-events"))
 )  # flush the batches to the Sink
