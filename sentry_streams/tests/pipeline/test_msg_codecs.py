6bda28f4e2c8f8617a1a733a52eed08d45dad7b2
victoria-yining-huang <victoria.huang777@gmail.com>
2025-06-04 14:12:42 -0400

    add wip
    add all draft
    
    remove prints
    
    frmt
    
    move exception block
    
    frmt
    
    typing
    
    typing
    
    input output type
    
    typing
    
    add generic tout
    
    push for riya
    
    type fixes
    
    remove comment
    
    add unit test
    
    add unit test
    
    use real classes
    
    dont cast
    
    fix module path
    
    hardcode a real codec
    
    use real things from sentry kafka schema

--- sentry_streams/tests/pipeline/test_msg_codecs.py
+++ sentry_streams/tests/pipeline/test_msg_codecs.py
@@ -1,8 +1,14 @@
 import json
 from datetime import datetime
+from importlib import resources
+from typing import Sequence
 from unittest.mock import MagicMock
 
-from sentry_streams.pipeline.msg_codecs import msg_serializer
+from pytest import MonkeyPatch
+from sentry_kafka_schemas.codecs.json import JsonCodec
+
+from sentry_streams.pipeline.message import PyMessage
+from sentry_streams.pipeline.msg_codecs import batch_msg_parser, msg_serializer
 
 
 def test_msg_serializer_default_isoformat() -> None:
@@ -24,3 +30,37 @@ def test_msg_serializer_custom_dt_format() -> None:
     result_bytes = msg_serializer(mock_msg, dt_format=dt_format)
     result_str = result_bytes.decode("utf-8")
     assert json.loads(result_str) == {"timestamp": dt.strftime(dt_format)}
+
+
+def test_batch_msg_parser_nominal_case(monkeypatch: MonkeyPatch) -> None:
+    with (
+        resources.files("sentry_kafka_schemas.examples.ingest-metrics.1")
+        .joinpath("base64-set.json")
+        .open("r") as f
+    ):
+        data = json.load(f)
+    expected = [data]
+
+    payload: Sequence[bytes] = [json.dumps(data).encode("utf-8")]
+
+    with (
+        resources.files("sentry_kafka_schemas.schemas")
+        .joinpath("ingest-metrics.v1.schema.json")
+        .open("r") as f
+    ):
+        json_schema = json.load(f)
+
+    monkeypatch.setattr(
+        "sentry_streams.pipeline.msg_codecs._get_codec_from_msg",
+        lambda _: JsonCodec(json_schema=json_schema),
+    )
+
+    msg = PyMessage(
+        payload=payload,
+        schema="test-schema",
+        headers=[],
+        timestamp=0.0,
+    )
+
+    result = batch_msg_parser(msg)
+    assert result == expected
