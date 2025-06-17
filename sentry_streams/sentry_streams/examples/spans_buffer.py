7508cd778e88b1d36f82ca74f1fe443fd4f9df3d
Evan Hicks <evan.hicks@sentry.io>
2025-06-17 12:17:07 -0400

    fix(examples): Rewrite examples using the chain style API
    Rewrite the examples to use the chain style API. The billing example is migrated in a separate PR.
    Remove some of the helper files since they weren't being used outside of the example and weren't
    very complicated.

--- sentry_streams/sentry_streams/examples/spans_buffer.py
+++ sentry_streams/sentry_streams/examples/spans_buffer.py
@@ -1,29 +1,93 @@
+import json
+from dataclasses import dataclass
 from datetime import timedelta
+from typing import Any, Self
 
-from sentry_streams.examples.spans import SpansBuffer, build_segment_json, build_span
-from sentry_streams.pipeline.pipeline import (
-    Aggregate,
-    Map,
-    Pipeline,
-    StreamSink,
-    StreamSource,
+from sentry_streams.pipeline import Map, Reducer, StreamSink, streaming_source
+from sentry_streams.pipeline.function_template import (
+    Accumulator,
 )
+from sentry_streams.pipeline.message import Message
 from sentry_streams.pipeline.window import TumblingWindow
 
-pipeline = Pipeline()
 
-source = StreamSource(
-    name="myinput",
-    ctx=pipeline,
-    stream_name="events",
-)
+@dataclass
+class Span:
+    span_id: int
+    trace_id: int
+    duration: int
+    timestamp: int
 
-map = Map(
-    name="mymap",
-    ctx=pipeline,
-    inputs=[source],
-    function=build_span,
-)
+    def to_dict(self) -> dict[str, int]:
+        return {
+            "span_id": self.span_id,
+            "trace_id": self.trace_id,
+            "duration": self.duration,
+            "timestamp": self.timestamp,
+        }
+
+
+def build_span(value: Message[bytes]) -> Span:
+    """
+    Build a Span object from a JSON str
+    """
+
+    d: dict[str, Any] = json.loads(value.payload)
+
+    return Span(d["span_id"], d["trace_id"], d["duration"], d["timestamp"])
+
+
+@dataclass
+class Segment:
+    total_duration: int
+    spans: list[Span]
+
+
+def build_segment_json(message: Message[Segment]) -> str:
+    """
+    Build a JSON str from a Segment
+    """
+    value = message.payload
+    d = {"segment": [], "total_duration": value.total_duration}
+
+    for span in value.spans:
+        span_d = span.to_dict()
+
+        assert isinstance(d["segment"], list)
+        d["segment"].append(span_d)
+
+    return json.dumps(d)
+
+
+class SpansBuffer(Accumulator[Message[Span], Segment]):
+    """
+    Ingests spans into a window. Builds a Segment from each
+    window, which contains the list of Spans seen as well
+    as the total duration across Spans.
+
+    TODO: Group by trace_id
+    """
+
+    def __init__(self) -> None:
+        self.spans_list: list[Span] = []
+        self.total_duration = 0
+
+    def add(self, value: Message[Span]) -> Self:
+        self.spans_list.append(value.payload)
+        self.total_duration += value.payload.duration
+
+        return self
+
+    def get_value(self) -> Segment:
+
+        return Segment(self.total_duration, self.spans_list)
+
+    def merge(self, other: Self) -> Self:
+        self.spans_list = self.spans_list + other.spans_list
+        self.total_duration = self.total_duration + other.total_duration
+
+        return self
+
 
 # A sample window.
 # Windows are open for 5 seconds max
@@ -35,24 +99,31 @@ reduce_window = TumblingWindow(window_size=timedelta(seconds=5))
 # Make the trigger and closing windows synonymous, both
 # apparent in the API and as part of implementation
 
-reduce = Aggregate(
-    name="myreduce",
-    ctx=pipeline,
-    inputs=[map],
-    window=reduce_window,
-    aggregate_func=SpansBuffer,
-)
-
-map_str = Map(
-    name="map_str",
-    ctx=pipeline,
-    inputs=[reduce],
-    function=build_segment_json,
-)
-
-sink = StreamSink(
-    name="kafkasink",
-    ctx=pipeline,
-    inputs=[map_str],
-    stream_name="transformed-events",
+pipeline = (
+    streaming_source(name="myinput", stream_name="events")
+    .apply(
+        "mymap",
+        Map(
+            function=build_span,
+        ),
+    )
+    .apply(
+        "myreduce",
+        Reducer(
+            window=reduce_window,
+            aggregate_func=SpansBuffer,
+        ),
+    )
+    .apply(
+        "map_str",
+        Map(
+            function=build_segment_json,
+        ),
+    )
+    .sink(
+        "kafkasink",
+        StreamSink(
+            stream_name="transformed-events",
+        ),
+    )
 )
--- sentry_streams/sentry_streams/examples/word_counter.py
+++ sentry_streams/sentry_streams/examples/word_counter.py
@@ -1,59 +1,85 @@
+import json
+from typing import Self
+
 from sentry_streams.examples.word_counter_fn import (
-    EventsPipelineFilterFunctions,
-    EventsPipelineMapFunction,
     GroupByWord,
-    WordCounter,
 )
-from sentry_streams.pipeline.pipeline import (
-    Aggregate,
-    Filter,
-    Map,
-    Pipeline,
-    StreamSink,
-    StreamSource,
-)
+from sentry_streams.pipeline import Filter, Map, Reducer, streaming_source
+from sentry_streams.pipeline.chain import StreamSink
+from sentry_streams.pipeline.function_template import Accumulator
+from sentry_streams.pipeline.message import Message
 from sentry_streams.pipeline.window import TumblingWindow
 
-# pipeline: special name
-pipeline = Pipeline()
 
-source = StreamSource(
-    name="myinput",
-    ctx=pipeline,
-    stream_name="events",
-)
+def simple_filter(value: Message[bytes]) -> bool:
+    d = json.loads(value.payload)
+    return True if "name" in d else False
 
-filter = Filter(
-    name="myfilter",
-    ctx=pipeline,
-    inputs=[source],
-    function=EventsPipelineFilterFunctions.simple_filter,
-)
 
-map = Map(
-    name="mymap",
-    ctx=pipeline,
-    inputs=[filter],
-    function=EventsPipelineMapFunction.simple_map,
-)
+def simple_map(value: Message[bytes]) -> tuple[str, int]:
+    d = json.loads(value.payload)
+    word: str = d.get("word", "null_word")
+
+    return (word, 1)
+
+
+class WordCounter(Accumulator[Message[tuple[str, int]], str]):
+
+    def __init__(self) -> None:
+        self.tup = ("", 0)
+
+    def add(self, value: Message[tuple[str, int]]) -> Self:
+        self.tup = (value.payload[0], self.tup[1] + value.payload[1])
+
+        return self
+
+    def get_value(self) -> str:
+        return f"{self.tup[0]} {self.tup[1]}"
+
+    def merge(self, other: Self) -> Self:
+        first = self.tup[0] + other.tup[0]
+        second = self.tup[1] + other.tup[1]
+
+        self.tup = (first, second)
+
+        return self
+
 
 # A sample window.
 # Windows are assigned 3 elements.
 # TODO: Get the parameters for window in pipeline configuration.
 reduce_window = TumblingWindow(window_size=3)
 
-reduce: Aggregate[int, tuple[str, int], str] = Aggregate(
-    name="myreduce",
-    ctx=pipeline,
-    inputs=[map],
-    window=reduce_window,
-    aggregate_func=WordCounter,
-    group_by_key=GroupByWord(),
-)
-
-sink = StreamSink(
-    name="kafkasink",
-    ctx=pipeline,
-    inputs=[reduce],
-    stream_name="transformed-events",
+# pipeline: special name
+pipeline = (
+    streaming_source(
+        name="myinput",
+        stream_name="events",
+    )
+    .apply(
+        "myfilter",
+        Filter(
+            function=simple_filter,
+        ),
+    )
+    .apply(
+        "mymap",
+        Map(
+            function=simple_map,
+        ),
+    )
+    .apply(
+        "myreduce",
+        Reducer(
+            window=reduce_window,
+            aggregate_func=WordCounter,
+            group_by_key=GroupByWord(),
+        ),
+    )
+    .sink(
+        "kafkasink",
+        StreamSink(
+            stream_name="transformed-events",
+        ),
+    )
 )
