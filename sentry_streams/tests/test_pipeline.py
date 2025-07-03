aa4950b4dd7252db61d6a027b86654b06986fd66
Evan Hicks <evan.hicks@sentry.io>
2025-07-03 14:57:19 -0400

    fix tests

--- sentry_streams/tests/test_pipeline.py
+++ sentry_streams/tests/test_pipeline.py
@@ -225,7 +225,7 @@ def test_add_empty_pipeline_to_empty_pipeline() -> None:
 
     pipeline1.add(pipeline2)
 
-    assert len(pipeline1.steps) == 0
+    assert len(pipeline1.steps) == 2
     assert len(pipeline1.sources) == 0
     assert len(pipeline1.incoming_edges) == 0
     assert len(pipeline1.outgoing_edges) == 0
@@ -239,7 +239,7 @@ def test_add_to_empty() -> None:
     )
     pipeline1.add(pipeline2)
 
-    assert len(pipeline1.steps) == 2
+    assert len(pipeline1.steps) == 3
     assert len(pipeline1.sources) == 1
     assert pipeline1.sources[0].name == "source"
     assert pipeline1.incoming_edges["sink"] == ["source"]
@@ -259,7 +259,7 @@ def test_add_multi_pipeline() -> None:
     )
     pipeline1.add(pipeline2)
 
-    assert len(pipeline1.steps) == 4
+    assert len(pipeline1.steps) == 5
     assert len(pipeline1.sources) == 2
     assert {source.name for source in pipeline1.sources} == {"source1", "source2"}
     assert pipeline1.incoming_edges["sink1"] == ["source1"]
