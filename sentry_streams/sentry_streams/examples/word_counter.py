26daf2d39726a6b2f532b23dbbeeecc0c636c0df
Evan Hicks <evanh@users.noreply.github.com>
2025-07-08 11:56:15 -0400

    fix(examples): Rewrite examples using the chain style API (#147)
    Rewrite the examples to use the chain style API. The billing example is
    migrated in a separate PR. Remove some of the helper files since they
    weren't being used outside of the example and weren't very complicated.
    
    Also skip the `alerts.py` file since it uses a FlatMap, and that has to
    be implemented in a separate PR.

--- sentry_streams/sentry_streams/examples/word_counter.py
+++ sentry_streams/sentry_streams/examples/word_counter.py
@@ -1,59 +1,48 @@
-from sentry_streams.examples.word_counter_fn import (
-    EventsPipelineFilterFunctions,
-    EventsPipelineMapFunction,
+from sentry_streams.examples.word_counter_helpers import (
     GroupByWord,
     WordCounter,
+    simple_filter,
+    simple_map,
 )
-from sentry_streams.pipeline.pipeline import (
-    Aggregate,
-    Filter,
-    Map,
-    Pipeline,
-    StreamSink,
-    StreamSource,
-)
+from sentry_streams.pipeline import Filter, Map, Reducer, streaming_source
+from sentry_streams.pipeline.chain import StreamSink
 from sentry_streams.pipeline.window import TumblingWindow
-
-# pipeline: special name
-pipeline = Pipeline()
-
-source = StreamSource(
-    name="myinput",
-    ctx=pipeline,
-    stream_name="events",
-)
-
-filter = Filter(
-    name="myfilter",
-    ctx=pipeline,
-    inputs=[source],
-    function=EventsPipelineFilterFunctions.simple_filter,
-)
-
-map = Map(
-    name="mymap",
-    ctx=pipeline,
-    inputs=[filter],
-    function=EventsPipelineMapFunction.simple_map,
-)
 
 # A sample window.
 # Windows are assigned 3 elements.
 # TODO: Get the parameters for window in pipeline configuration.
 reduce_window = TumblingWindow(window_size=3)
 
-reduce: Aggregate[int, tuple[str, int], str] = Aggregate(
-    name="myreduce",
-    ctx=pipeline,
-    inputs=[map],
-    window=reduce_window,
-    aggregate_func=WordCounter,
-    group_by_key=GroupByWord(),
-)
-
-sink = StreamSink(
-    name="kafkasink",
-    ctx=pipeline,
-    inputs=[reduce],
-    stream_name="transformed-events",
+# pipeline: special name
+pipeline = (
+    streaming_source(
+        name="myinput",
+        stream_name="events",
+    )
+    .apply(
+        "myfilter",
+        Filter(
+            function=simple_filter,
+        ),
+    )
+    .apply(
+        "mymap",
+        Map(
+            function=simple_map,
+        ),
+    )
+    .apply(
+        "myreduce",
+        Reducer(
+            window=reduce_window,
+            aggregate_func=WordCounter,
+            group_by_key=GroupByWord(),
+        ),
+    )
+    .sink(
+        "kafkasink",
+        StreamSink(
+            stream_name="transformed-events",
+        ),
+    )
 )
