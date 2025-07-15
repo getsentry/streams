59b3a6b94c0509764a78c6f7301b7235b29e5e10
Evan Hicks <evanh@users.noreply.github.com>
2025-07-15 10:21:29 -0400

    feat(tests): Add an integration test for an example pipeline (#178)
    This sets up a test framework for doing E2E tests on a pipeline. This
    test will start the runner in
    a subprocess, send the generated messages to the source, and check the
    outputs are correct.
    
    This also adds a CI job to run these tests.

--- sentry_streams/sentry_streams/examples/simple_map_filter.py
+++ sentry_streams/sentry_streams/examples/simple_map_filter.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-
 from sentry_kafka_schemas.schema_types.ingest_metrics_v1 import IngestMetric
 
-from sentry_streams.examples.transform_metrics import transform_msg
-from sentry_streams.pipeline.message import Message
+from sentry_streams.examples.transform_metrics import filter_events, transform_msg
 from sentry_streams.pipeline.pipeline import (
     Filter,
     Map,
@@ -12,18 +9,6 @@ from sentry_streams.pipeline.pipeline import (
     StreamSink,
     streaming_source,
 )
-
-
-def filter_events(msg: Message[IngestMetric]) -> bool:
-    return bool(msg.payload["type"] == "c")
-
-
-def generate_files() -> str:
-    now = datetime.now()
-    cur_time = now.strftime("%H:%M:%S")
-
-    return f"file_{cur_time}.txt"
-
 
 pipeline = streaming_source(name="myinput", stream_name="ingest-metrics")
 
