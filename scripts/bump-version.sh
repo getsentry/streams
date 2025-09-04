dc5bfde9d346b1bfafe76db32baad6f0f94e0011
victoria-yining-huang <44307912+victoria-yining-huang@users.noreply.github.com>
2025-09-04 11:10:45 -0400

    fix(release): bump version script recognizing white spaces (#207)
    the bump-version.sh script did not recognize the indentation added from
    this PR
    https://github.com/getsentry/streams/commit/d828ca8ea5739cee42792e86dca1c9fcefba2770
    
    so i fixed the bump-version script to recognize any indentation and also
    formatted the pyproject.toml file back to no indentation because the
    bump-version script would not add the new version with the same
    indentation and it's ugly.
    
    once this is in we should finally be able to release a working version

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -8,9 +8,9 @@ echo "Current version: $OLD_VERSION"
 echo "Bumping version: $NEW_VERSION"
 
 function replace() {
-    ! grep "$2" $3
-    perl -i -pe "s/$1/$2/g" $3
-    grep "$2" $3  # verify that replacement was successful
+    ! grep "$2" "$3"
+    perl -i -pe "s/$1/$2/g" "$3"
+    grep "$2" "$3"  # verify that replacement was successful
 }
 
-replace "version = \"[0-9.]+\""  "version = \"$NEW_VERSION\"" pyproject.toml
+replace '^\s*version\s*=\s*"[0-9.]+"' "version = \"$NEW_VERSION\"" pyproject.toml
