8f8f3fd0e502199327f47111eb5deaeb2857b016
Filippo Pacifici <fpacifici@sentry.io>
2026-01-09 16:50:34 -0800

    Better docs

--- sentry_streams_k8s/README.md
+++ sentry_streams_k8s/README.md
@@ -2,4 +2,19 @@
 
 Kubernetes integration for Sentry Streams.
 
-This package provides tools and utilities for deploying and managing Sentry Streams pipelines on Kubernetes.
+Sentry Streams contains the streaming platform. That is everything that runs
+the streaming applications. Sentry Streams is agnostic to the way the application
+is deployed in production or in any environment.
+
+This package instead provides the automation and the infrastructure to deploy
+Sentry Streams in a Kubernetes environment.
+
+At the moment it targets Sentry production environment that is based on
+[sentry-kube](https://github.com/getsentry/sentry-infra-tools), though in the
+future we expect to have something more Sentry agnostic like Helm charts and
+a Kubernetes operator.
+
+The main component is the Consumer macro. This is a sentry-kube external macro
+that can be used inside the sentry-kube jinja template in the ops repo.
+This takes the basic deployment infrastructure (application specific) and
+fills it with containers and other resources to run the streaming platform.
--- sentry_streams_k8s/sentry_streams_k8s/consumer.py
+++ sentry_streams_k8s/sentry_streams_k8s/consumer.py
@@ -13,15 +13,31 @@ class Consumer(ExternalMacro):
     basic infrastructure. This can include COGS labeling, nodepool config,
     some sidecars, etc.
 
-    This macros fills it in with the straming platform content: containers, volumes,
+    This macros fills it in with the streaming platform content: containers, volumes,
     configmap, naming conventions, etc.
     A similar pattern is followed by the Flink python operator, the user can
     provide a deployment template in the CRD, the flink operator fills it in
     with Flink.
 
-    The goal of this is to standardie the the deployment of streaming platform
+    The goal of this is to standardize the deployment of streaming platform
     consumer while still sticking to the Sentry Kubernetes infrastructure based
     on client side rendering of templates and sentry-kube macros.
+
+    This would be used like this in a jinja template:
+
+    ```
+    {% import '_deployment_template.j2' as deployment -%}
+    {% set deployment = deployment.deployment() %}
+    {{ render_external(
+            "sentry_streams_k8s.consumer.Consumer",
+            {
+                "deployment_template": deployment,
+                "pipeline_module": "sbc.profiles",
+                "pipeline_config_file": "sbc_profiles.yaml",
+            }
+        )
+    }}
+    ```
     """
 
     @staticmethod
