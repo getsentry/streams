ac0d2b1745ae329d5d5ecc2846c4fe73d866b654
Filippo Pacifici <fpacifici@sentry.io>
2026-04-16 09:42:35 -0700

    PAssthrough

--- sentry_streams/sentry_streams/examples/filter_profile.py
+++ sentry_streams/sentry_streams/examples/filter_profile.py
@@ -1,19 +1,8 @@
 from sentry_streams.pipeline.pipeline import (
-    Batch,
     DevNullSink,
-    HeadersFilter,
     streaming_source,
 )
 
-pipeline = (
-    streaming_source(name="myinput", stream_name="snuba-items")
-    .apply(
-        HeadersFilter(
-            name="spans_filter",
-            header_name="item_type",
-            value=1,
-        )
-    )
-    .apply(Batch(name="mybatch", batch_size=45000))
-    .sink(DevNullSink(name="devnull"))
+pipeline = streaming_source(name="myinput", stream_name="snuba-items").sink(
+    DevNullSink(name="devnull")
 )
