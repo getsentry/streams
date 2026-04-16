f76e793449e0ddacb1c64beeecee6920ad2b661e
Filippo Pacifici <fpacifici@sentry.io>
2026-04-16 09:46:51 -0700

    Remove filter again

--- sentry_streams/sentry_streams/examples/filter_profile.py
+++ sentry_streams/sentry_streams/examples/filter_profile.py
@@ -1,8 +1,11 @@
 from sentry_streams.pipeline.pipeline import (
+    Batch,
     DevNullSink,
     streaming_source,
 )
 
-pipeline = streaming_source(name="myinput", stream_name="snuba-items").sink(
-    DevNullSink(name="devnull")
+pipeline = (
+    streaming_source(name="myinput", stream_name="snuba-items")
+    .apply(Batch(name="mybatch", batch_size=45000))
+    .sink(DevNullSink(name="devnull"))
 )
