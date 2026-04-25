9eaf68f214d76e7167f7647266942e3671f8de80
Filippo Pacifici <fpacifici@sentry.io>
2026-04-24 22:56:21 -0700

    Add a parser

--- sentry_streams/sentry_streams/adapters/arroyo/steps_chain.py
+++ sentry_streams/sentry_streams/adapters/arroyo/steps_chain.py
@@ -43,7 +43,7 @@ def fake_transform(message: Message[Any]) -> Message[Any]:
     next_msg = message
     # msg_size = get_size(next_msg.payload) if hasattr(next_msg, "payload") else None
     msg_size = 100
-    # start_time = input_metrics("fake_step", msg_size)
+    start_time = input_metrics("fake_step", msg_size)
     has_error = output_size = None
     try:
         result = msg_parser(next_msg)
@@ -53,8 +53,7 @@ def fake_transform(message: Message[Any]) -> Message[Any]:
         has_error = str(e.__class__.__name__)
         raise e
     finally:
-        # output_metrics("fake_step", has_error, start_time, output_size)
-        pass
+        output_metrics("fake_step", has_error, start_time, output_size)
 
     ret = result
 
