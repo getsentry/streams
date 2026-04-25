d2152f785506990f2664322837b51562e45d9dbc
Filippo Pacifici <fpacifici@sentry.io>
2026-04-24 22:04:41 -0700

    Add a parser

--- sentry_streams/sentry_streams/adapters/arroyo/steps_chain.py
+++ sentry_streams/sentry_streams/adapters/arroyo/steps_chain.py
@@ -41,22 +41,7 @@ def output_metrics(
 
 def fake_transform(message: Message[Any]) -> Message[Any]:
     next_msg = message
-    # msg_size = get_size(next_msg.payload) if hasattr(next_msg, "payload") else None
-    msg_size = 100
-    # start_time = input_metrics("fake_transform", msg_size)
-    has_error = output_size = None
-    try:
-        result = msg_parser(next_msg)
-        # output_size = get_size(result)
-        output_size = 100
-        ret = result
-    except Exception as e:
-        has_error = str(e.__class__.__name__)
-        raise e
-    finally:
-        # output_metrics("fake_transform", has_error, start_time, output_size)
-        pass
-
+    ret = msg_parser(message)
     if isinstance(ret, bytes):
         # If `ret`` is bytes then function is Callable[Message[TMapIn], bytes].
         # Thus TMapOut = bytes.
