8c0b266faaa3dab4e5e319015026344652cb7efc
Filippo Pacifici <fpacifici@sentry.io>
2026-04-26 18:06:18 -0700

    Try to add batch

--- sentry_streams/sentry_streams/adapters/arroyo/fake_delegate.py
+++ sentry_streams/sentry_streams/adapters/arroyo/fake_delegate.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
-from typing import Iterable, MutableSequence, Set, Tuple
+import time
+from typing import Any, Iterable, MutableSequence, Sequence, Set, Tuple
 
 from sentry_streams.adapters.arroyo.rust_step import (
     Committable,
@@ -7,8 +8,37 @@ from sentry_streams.adapters.arroyo.rust_step import (
     SingleMessageOperatorDelegate,
 )
 from sentry_streams.examples.transform_metrics import do_nothing
+from sentry_streams.metrics.stats import get_stats
 from sentry_streams.pipeline.message import PipelineMessage, PyMessage, RustMessage
 from sentry_streams.rust_streams import PyWatermark
+
+
+class Messagebatch:
+
+    def __init__(self, size: int, time_sec: float) -> None:
+        self.size = size
+        self.time_sec = time_sec
+        self.messages: MutableSequence[Any] = []
+        self.committable: Committable = {}
+        self._last_flush_time: float | None = None
+
+    def add_message(self, message: RustMessage, committable: Committable) -> None:
+        if self._last_flush_time is None:
+            self._last_flush_time = time.time()
+        self.messages.append(message.payload)
+        self.committable.update(committable)
+
+    def flush(self) -> Tuple[Sequence[Any], Committable] | None:
+        if self._last_flush_time is None:
+            self._last_flush_time = time.time()
+        if len(self.messages) < self.size or time.time() - self._last_flush_time < self.time_sec:
+            return None
+
+        messages = self.messages
+        self.messages = []
+        self.committable = {}
+        self._last_flush_time = time.time()
+        return messages, self.committable
 
 
 class FakeOperatorDelegate(RustOperatorDelegate):
@@ -19,7 +49,8 @@ class FakeOperatorDelegate(RustOperatorDelegate):
         # globbed_committable is the combined committable of all messages polled
         # from the OutputRetriever
         self.__globbed_committable: Committable = {}
-        self.__messages: MutableSequence[Tuple[PipelineMessage, Committable]] = []
+        self.__batch = Messagebatch(size=50000, time_sec=3.0)
+        self.stats = get_stats()
 
     def __should_send_watermark(self, watermark: PyWatermark) -> bool:
         """
@@ -49,25 +80,27 @@ class FakeOperatorDelegate(RustOperatorDelegate):
         """
         # TODO: ensure watermarks leave the delegate in the same order they entered it
         ret: MutableSequence[Tuple[PipelineMessage, Committable]] = []
-        for message, committable in self.__messages:
-            ret.append((message, committable))
-            self.__globbed_committable.update(committable)
-            watermarks = self.__watermarks.copy()
-            for wm in watermarks:
-                if self.__should_send_watermark(wm):
-                    ret.append((wm, wm.committable))
-                    self.__watermarks.remove(wm)
-        self.__messages = []
+        flushed = self.__batch.flush()
+
+        if not flushed:
+            return ret
+
+        msg = PyMessage(flushed[0], [], time.time(), None).to_inner()
+        ret.append((msg, flushed[1]))
+        self.__globbed_committable.update(flushed[1])
+        watermarks = self.__watermarks.copy()
+        for wm in watermarks:
+            if self.__should_send_watermark(wm):
+                ret.append((wm, wm.committable))
+                self.__watermarks.remove(wm)
         return ret
 
     def submit(self, message: PipelineMessage, committable: Committable) -> None:
+        self.stats.step_exec("fake batch")
         if isinstance(message, PyWatermark):
             self.__watermarks.add(message)
         else:
-            processed = PyMessage(
-                {"size": len(message.payload)}, message.headers, message.timestamp, message.schema
-            )
-            self.__messages.append((processed.to_inner(), committable))
+            self.__batch.add_message(message, committable)
 
     def poll(self) -> Iterable[Tuple[PipelineMessage, Committable]]:
         return self.__yield_messages()
