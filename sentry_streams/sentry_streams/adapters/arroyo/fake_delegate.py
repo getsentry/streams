04302abcaf14f0fadd69e76057f0f9688b020d4d
Filippo Pacifici <fpacifici@sentry.io>
2026-04-26 17:19:07 -0700

    Try to add batch

--- sentry_streams/sentry_streams/adapters/arroyo/fake_delegate.py
+++ sentry_streams/sentry_streams/adapters/arroyo/fake_delegate.py
@@ -4,10 +4,16 @@ from typing import Tuple
 from sentry_streams.adapters.arroyo.rust_step import Committable, SingleMessageOperatorDelegate
 from sentry_streams.examples.transform_metrics import do_nothing
 from sentry_streams.pipeline.message import PipelineMessage, PyMessage, RustMessage
+from sentry_streams.rust_streams import PyWatermark
 
 
 class FakeOperatorDelegate(SingleMessageOperatorDelegate):
-    def _process_message(self, msg: RustMessage, committable: Committable) -> RustMessage | None:
+    def _process_message(
+        self, msg: PipelineMessage, committable: Committable
+    ) -> RustMessage | None:
+        if isinstance(msg, PyWatermark):
+            return msg
+
         copy = deepcopy(msg.payload)
         payload = {"size": len(copy)}
 
