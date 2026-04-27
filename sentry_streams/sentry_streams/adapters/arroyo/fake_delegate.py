897635d16a7de13ce0da9a17d0b95272f21a2aac
Filippo Pacifici <fpacifici@sentry.io>
2026-04-26 17:13:09 -0700

    Try to add batch

--- sentry_streams/sentry_streams/adapters/arroyo/fake_delegate.py
+++ sentry_streams/sentry_streams/adapters/arroyo/fake_delegate.py
@@ -1,12 +1,17 @@
+from copy import deepcopy
 from typing import Tuple
 
 from sentry_streams.adapters.arroyo.rust_step import Committable, SingleMessageOperatorDelegate
-from sentry_streams.pipeline.message import PipelineMessage, RustMessage
+from sentry_streams.examples.transform_metrics import do_nothing
+from sentry_streams.pipeline.message import PipelineMessage, PyMessage, RustMessage
 
 
 class FakeOperatorDelegate(SingleMessageOperatorDelegate):
     def _process_message(self, msg: RustMessage, committable: Committable) -> RustMessage | None:
-        return msg
+        copy = deepcopy(msg.payload)
+        payload = {"size": len(copy)}
+
+        return PyMessage(payload, msg.headers, msg.timestamp, msg.schema).to_inner()
 
 
 class FakeOperatorDelegateFactory:
