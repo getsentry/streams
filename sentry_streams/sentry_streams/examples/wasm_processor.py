5f95879f7f87778403c245a85e2ba2302fe18734
Filippo Pacifici <fpacifici@sentry.io>
2026-05-21 18:07:45 -0700

    Fix pipeline

--- sentry_streams/sentry_streams/examples/wasm_processor.py
+++ sentry_streams/sentry_streams/examples/wasm_processor.py
@@ -4,14 +4,13 @@ from sentry_streams.pipeline.pipeline import (
     streaming_source,
 )
 
-pipeline = streaming_source(name="myinput", stream_name="ingest-events")
+pipeline = streaming_source(name="myinput", stream_name="ingest-metrics")
 
 (
     pipeline.apply(
         WasmProcessor(
             "wasm",
-            module_path="examples/wasm_guest/dist/plugin.wasm",
+            module_path="/Users/filippopacifici/code/streams/examples/wasm_guest/dist/plugin.wasm",
         )
-    )
-    .sink(StreamSink("mysink", stream_name="processed-events"))
+    ).sink(StreamSink("mysink", stream_name="processed-profiles"))
 )
